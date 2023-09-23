2e8ca143f0e94fd4c8bfd5a0956747388b373404
Егор Скоморохов <egorskomorohov020606@gmail.com>
2023-09-23 11:33:26 +0500

    добавил пятую практику

---
+++ pz_05/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace pz_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int n = int.Parse(Console.ReadLine()); // вводим число
+            if (n < 0) // делаем проверку
+            {
+                Console.WriteLine("Вы ввели отрицательное число!");
+            }
+            else
+            {
+                int sum = 0;
+                int i = 0;
+                while (i <= n) // считаем сумму
+                {
+                    sum += i;
+                    i++;
+                }
+                Console.WriteLine($"Сумма чисел от 0 до {n}: {sum}"); // выводим значения
+            }
+
+        }
+    }
+}
