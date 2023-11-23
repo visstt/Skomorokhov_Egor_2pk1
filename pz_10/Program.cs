3cf5c4310cbd866f9dee750cec6d9fa7f3bdf9c3
Егор Скоморохов <egorskomorohov020606@gmail.com>
2023-11-23 23:20:02 +0500

    добавил десятую практику

---
+++ pz_10/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace pz_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите почты:");
+            string mails = Console.ReadLine();
+
+            string[] mailArray = mails.Split();
+            foreach (string mail in mailArray)
+            {
+                int atIndex = mail.IndexOf('@');
+                if (atIndex != -1)
+                {
+                    string name = mail.Substring(0, atIndex);
+                    Console.WriteLine("Привет, " + name);
+                }
+            }
+        }
+    }
+}
