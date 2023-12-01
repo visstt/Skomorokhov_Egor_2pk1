b4e05a8765b1af063ed76ddd84e9c39c75a6100e
Егор Скоморохов <egorskomorohov020606@gmail.com>
2023-12-01 18:49:32 +0500

    добавил 12 практику

---
+++ pz_12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pz_12
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] inputArray = { "hello", "test", "string" }; // задаем массив слов
+            string[] result = MaxLengthArray(inputArray); // Вызываем метод MaxLengthArray, передавая ему массив inputArray
+            foreach (var item in result) // Перебираем все строки в массиве result
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        static string[] MaxLengthArray(string[] arr) // Определяем метод MaxLengthArray
+        {
+            int maxLength = arr.Max(item => item.Length); // Ищем максимальную длину строки в массиве arr
+            return arr.Select(item => item.PadRight(maxLength, '#')).ToArray(); // Возвращаем массив, содержащий все преобразованные строки
+        }
+    }
+}
