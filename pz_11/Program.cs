1d7d772a3a7a420e2baecc9f4f84c82a52770fa5
Егор Скоморохов <egorskomorohov020606@gmail.com>
2023-12-01 18:34:47 +0500

    добавил 11 практику

--- pz_11/Program.cs
+++ pz_11/Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Введите x: ");
+            Console.Write("Введите x: ");              // вводим все нужные переменные
             int x = int.Parse(Console.ReadLine());
             Console.Write("Введите y: ");
             int y = int.Parse(Console.ReadLine());
-            Quarter(x, y);
+            Quarter(x, y);   // обращаемся к методу
 
         }
 
-        static void Quarter(int x, int y)
+        static void Quarter(int x, int y)  // вызываем метод который выводит значения, делая проверку на четверть
         {
             if (x > 0 && y > 0)
             {
