945939fd2b8da715fbb44c5ee7a73fd77c499f32
Егор Скоморохов <egorskomorohov020606@gmail.com>
2023-12-01 18:32:41 +0500

    добавил 11 практику

---
+++ pz_11/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace pz_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите x: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите y: ");
+            int y = int.Parse(Console.ReadLine());
+            Quarter(x, y);
+
+        }
+
+        static void Quarter(int x, int y)
+        {
+            if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Первая четверть");
+            }
+            if (x < 0 && y > 0)
+            {
+                Console.WriteLine("Вторая четверть");
+            }
+            if (x < 0 && y < 0)
+            {
+                Console.WriteLine("Третья четверть");
+            }
+            if (x > 0 && y < 0)
+            {
+                Console.WriteLine("Четвертая четверть");
+            }
+        }
+    }
+}
